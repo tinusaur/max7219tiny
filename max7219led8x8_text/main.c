8fe1f9b5bd63d99d450db4f7cc723e51c1fb8516
boyanov <none@none>
2018-01-01 18:57:31 +0200

    Updated copyright info.

--- max7219led8x8_text/main.c
+++ max7219led8x8_text/main.c
@@ -5,7 +5,7 @@
  *
  * This is part of the Tinusaur/MAX7219LED8x8 project.
  *
- * Copyright (c) 2017 Neven Boyanov, The Tinusaur Team. All Rights Reserved.
+ * Copyright (c) 2018 Neven Boyanov, The Tinusaur Team. All Rights Reserved.
  * Distributed as open source software under MIT License, see LICENSE.txt file.
  * Retain in your source code the link http://tinusaur.org to the Tinusaur project.
  *
@@ -25,27 +25,27 @@
 #include <util/delay.h>
 #include <avr/pgmspace.h>
 
+#include "max7219led8x8/max7219led8x8.h"
+#include "max7219led8x8/max7219led8x8f.h"
 // If you need to change the ports for the DIN/CS/CLK you should do so
 // in the "max7219led8x8.h" source code file in the MAX7219LED8x8 library 
 // so it will take affect on all the code.
-
-#include "max7219led8x8/max7219led8x8.h"
-#include "max7219led8x8/max7219led8x8f.h"
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //                 ATtiny85
 //               +----------+   (-)--GND--
 //       (RST)---+ PB5  Vcc +---(+)--VCC--
-// ---[OWOWOD]---+ PB3  PB2 +--------CLK--
-// --------------+ PB4  PB1 +---------CS--
-// --------(-)---+ GND  PB0 +--------DIN--
+// ---[OWOWOD]---+ PB3  PB2 +---DIN-------
+// --------------+ PB4  PB1 +---CS--------
+// --------(-)---+ GND  PB0 +---CLK-------
 //               +----------+
 //              Tinusaur Board
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // ----------------------------------------------------------------------------
 
-#define MAX7219_SEG_NUM (4 + 1)	// Segments, number of 8x8 matrices + 1 extra
+#define MAX7219_SEG_NUM (1+1)	// Number of segments, i.e. number of 8x8 matrices. Increase this for cascade matrices.
+								// One extra element added to the buffer, for the "hidden" symbol that will scroll.
 #define MAX7219_BUFFER_SIZE	MAX7219_SEG_NUM * 8
 
 uint8_t max7219_buffer[MAX7219_BUFFER_SIZE];
@@ -72,8 +72,8 @@ int main(void) {
 			max7219b_char((MAX7219_SEG_NUM - 1) * 8, c);
 			max7219b_out();	// Output the buffer
 			for (uint8_t s = 0; s < 8; s++) {
-				max7219b_left();
-				max7219b_out();	// Output the buffer
+				max7219b_left();	// Scroll to the left.
+				max7219b_out();		// Manually output the buffer.
 				_delay_ms(40);
 			}
 		}
