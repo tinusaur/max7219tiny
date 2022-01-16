693d227560e85b1cba4506a92b18dbc281fab485
Neven Boyanov <boyanov@msn.com>
2022-01-16 17:52:20 +0200

    Updated testing script, simplified.

--- max7219tiny_test1/main.c
+++ max7219tiny_test1/main.c
@@ -16,41 +16,35 @@
 #include <util/delay.h>
 
 #include "max7219tiny/max7219tiny.h"
-// If you need to change the ports for the DIN/CS/CLK you should do so
-// in the "max7219tiny.h" source code file in the MAX7219tiny library
-// so it will take affect on all the code.
+// To reassign the DIN/CS/CLK to different I/O pins you must edit the library code
+// Find the definitions in the "max7219tiny.h" file in the MAX7219Tiny library.
+// IMPORTANT/NOTE: Do that ONLY of you know what you are doing!
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //                 ATtiny85
-//               +----------+   (-)--GND--
-//       (RST)---+ PB5  Vcc +---(+)--VCC--
-//  --[OWOWOD]---+ PB3  PB2 +------[DIN]--
-//  -------------+ PB4  PB1 +-------[CS]--
-//  --GND--(-)---+ GND  PB0 +------[CLK]--
+//               +----------+   (-)--GND---
+//       (RST)---+ PB5  Vcc +---(+)--VCC---
+//  --[OWOWOD]---+ PB3  PB2 +-------[CLK]--
+//  -------------+ PB4  PB1 +-------[DIN]--
+//  --GND--(-)---+ GND  PB0 +-------[CS]---
 //               +----------+
 //              Tinusaur Board
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-// ----------------------------------------------------------------------------
-
 int main(void) {
-
 	// ---- Initialization ----
 	max7219_init();
-
 	// ---- Main Loop ----
 	for (;;) {
-		for (uint8_t row = 0; row < 9; row++) {	// 9 times, so both rows will be output.
+		for (uint8_t r = 1; r <= 8; r++) { // 8 times, row values are 1 to 8.
 			uint8_t d = 1;
-			for (uint8_t i = 9; i > 0; i--) {	// 9 times, so the last is "0".
-				max7219_row(row, d);			// Display 1 pixel (1 bit)
-				max7219_row(row + 1, ~d);		// Display inverted value - 7 pixels.
+			for (uint8_t i = 9; i > 0; i--) { // 9 times, so the last value is "0".
+				max7219_row(r, d);
 				d = d << 1;
-				_delay_ms(50);
+				_delay_ms(100);
 			}
 		}
 	}
-
 	return 0; // Return the mandatory result value. It is "0" for success.
 }
 
