0f2e0ac68363031e1929557b84953e00b2fed46c
Neven Boyanov <boyanov@msn.com>
2022-01-23 12:23:02 +0200

    Text sample renamed (for consistency).

--- max7219tiny_tx1/main.c
+++ max7219tiny_tx1/main.c
@@ -16,58 +16,59 @@
 #include <util/delay.h>
 #include <avr/pgmspace.h>
 
+#include "tinyavrlib/scheduler.h"
+
 #include "max7219tiny/max7219tiny.h"
-#include "max7219tiny/max7219tinyf.h"
-// If you need to change the ports for the DIN/CS/CLK you should do so
-// in the "max7219tiny.h" source code file in the MAX7219tiny library 
-// so it will take affect on all the code.
+// To reassign the DIN/CS/CLK to different I/O pins you must edit the library code
+// Find the definitions in the "max7219tiny.h" file in the MAX7219Tiny library.
+// IMPORTANT/NOTE: Do that ONLY of you know what you are doing!
+#include "max7219tiny/max7219tinyfx.h"
+#include "max7219tiny/max7219tinytx.h"
+#include "max7219tiny/max7219tinytxf.h"
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //                 ATtiny85
-//               +----------+   (-)--GND--
-//       (RST)---+ PB5  Vcc +---(+)--VCC--
-//  --[OWOWOD]---+ PB3  PB2 +------[DIN]--
-//  -------------+ PB4  PB1 +-------[CS]--
-//  --GND--(-)---+ GND  PB0 +------[CLK]--
+//               +----------+   (-)--GND---
+//       (RST)---+ PB5  Vcc +---(+)--VCC---
+//  --[OWOWOD]---+ PB3  PB2 +-------[CLK]--
+//  -------------+ PB4  PB1 +-------[DIN]--
+//  --GND--(-)---+ GND  PB0 +-------[CS]---
 //               +----------+
 //              Tinusaur Board
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-#define MAX7219_SEG_NUM (1+1)	// The number of the segments. Increase this for multiple matrices.
-// NOTE: Add 1 extra element at the end of the buffer for a "hidden" symbol to scroll in.
-#define MAX7219_SEG_LAST (MAX7219_SEG_NUM - 1) * 8	// The index in the buffer of the last segment.
+#define MAX7219_SEG_NUM (4+1)	// The number of the segments. Increase this for multiple matrices.
+// NOTE: Add 1 for an extra element at the end of the buffer for a "hidden" symbol to scroll in.
 #define MAX7219_BUFFER_SIZE	MAX7219_SEG_NUM * 8		// The size of the buffer
 
 uint8_t max7219_buffer[MAX7219_BUFFER_SIZE];
 
 // ----------------------------------------------------------------------------
 
-// TODO: Move this function to a library.
+#define MAX7219FX_SCROLL_DELAY 2 // Delay in centisecond
 
-void max7219b_char(uint8_t x, uint8_t c) {
-	for(uint8_t i = 0; i <= 7; i++)
-		max7219b_col(x + i, pgm_read_byte(&max7219tiny_font[((c - 32) << 3) + i]));
-}
-
-// ----------------------------------------------------------------------------
+char *text_hello = "Hello, World! :)  ";
 
 int main(void) {
 	// ---- Initialization ----
+	scheduler_init(SCHEDULER_USERFUNC_NULL);
+	scheduler_reinit(SCHEDULER_TCCR0B_1024, SCHEDULER_OCR0A_MIN);	// Adjust, if necessary
+	scheduler_start();
 	max7219b_init(MAX7219_SEG_NUM, max7219_buffer, MAX7219_BUFFER_SIZE);
+	max7219b_scheduler();
+	max7219fx_init();
+	max7219tx_init(max7219tiny_font, ' ');
 	// ---- Main Loop ----
 	for (;;) {
-		for (uint8_t c = ' '; c <= 127; c++) {
-			max7219b_char((MAX7219_SEG_NUM - 1) * 8, c);
-			max7219b_out();	// Output the buffer
-			for (uint8_t s = 0; s < 8; s++) {
-				max7219b_left();	// Scroll to the left.
-				max7219b_out();		// Manually output the buffer.
-				_delay_ms(40);
-			}
+		// Print some text
+		char *text = text_hello;
+		while (*text) {
+			max7219tx_char(MAX7219_BUFFER_SIZE - 8, *text++);
+			max7219fx_left(6, MAX7219FX_SCROLL_DELAY);
 		}
+		_delay_ms(200);
 	}
-
-	return 0; // Return the mandatory result value. It is "0" for success.
+	return 0; // Return the mandatory result value.
 }
 
 // ============================================================================
